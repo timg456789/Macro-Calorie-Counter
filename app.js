dd40d77a0974937b43ae89c17792f4458ffbc5ef
Timothy Gonzalez <timg456789@yahoo.com>
2016-08-21 01:08:56 +0000

    get rid of cookie and body parser.

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
 var routes = require('./routes/index');
 
 var app = express();
@@ -11,11 +8,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', routes);
 
 // catch 404 and forward to error handler
